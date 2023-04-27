eff27663152b64ed6b59a335526c5b28bc2d7bc6
SarfDime <118685209+SarfDime@users.noreply.github.com>
2023-04-27 13:20:05 +0200

    different js structure

--- dropship/src/js/index.js
+++ dropship/src/js/index.js
@@ -1,7 +1,9 @@
+/// HEADER COMPONENTS
 import './header/_mode.js'
 import './header/_notification.js'
 import './header/_searchIndex.js'
 import './header/_categoriesDropDown.js'
 import './footer/_contact.js'
+//// SOMEHTING ELSE
 
 
