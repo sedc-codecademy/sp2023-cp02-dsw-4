684094abbda4a2d334cfda998e67b8e8ec3ea39a
SarfDime <118685209+SarfDime@users.noreply.github.com>
2023-05-01 20:12:46 +0200

    s

--- dropship/src/js/loginForm/_main.js
+++ dropship/src/js/loginForm/_main.js
@@ -1,6 +1,31 @@
-const loginDp = document.querySelector(".loginDropdown");
-const loginBtn = document.querySelector("#loginButton");
+const loginDp = document.querySelector(".loginDropdown")
+const loginBtn = document.querySelector("#loginButton")
+const cancelBtn = document.querySelector(".cancel")
+const registerUi = document.querySelector(".registerUi")
+const loginUi = document.querySelector(".loginUi")
+const registerBtn = document.querySelector(".registerButton")
+
+let rgOn = false
 
 loginBtn.addEventListener("click", () => {
-  loginDp.classList.toggle("on");
-});
+  loginDp.classList.toggle("on")
+})
+
+cancelBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  registerUi.classList.toggle('regOn')
+  loginUi.classList.toggle('loginOff')
+  rgOn = false
+})
+
+registerBtn.addEventListener('click', (e) => {
+  if(rgOn){
+    console.log('Sign up button clicked')
+    return
+  }
+  e.preventDefault()
+  registerUi.classList.toggle('regOn')
+  loginUi.classList.toggle('loginOff')
+  rgOn = true
+})
+
