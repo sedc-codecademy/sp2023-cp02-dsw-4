3dcbdf85549a3f7e4e3770d4c7d066475dfb1ef1
SarfDime <118685209+SarfDime@users.noreply.github.com>
2023-06-20 21:16:51 +0200

    s

--- dropship/src/js/header/_mode.js
+++ dropship/src/js/header/_mode.js
@@ -1,6 +1,8 @@
 const darkButton = document.querySelector(".darkButton")
 const dbutton = document.querySelector("#dbutton")
 
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches; ////////////
+
 themeCheck()
 
 export function themeCheck() {
@@ -13,20 +15,39 @@ export function themeCheck() {
     }
 }
 
-function applyDarkTheme() {
-    document.body.classList.toggle("dark-mode")
-    if (document.body.classList.contains("dark-mode")) {
-        localStorage.setItem("dark-mode", true)
-        dbutton.innerHTML = "light_mode"
-    } else {
-        localStorage.setItem("dark-mode", false)
-        dbutton.innerHTML = "dark_mode"
-    }
-    darkButton.disabled = false
-}
+// function applyDarkTheme() {
+//     document.body.classList.toggle("dark-mode")
+//     if (document.body.classList.contains("dark-mode")) {
+//         localStorage.setItem("dark-mode", true)
+//         dbutton.innerHTML = "light_mode"
+//     } else {
+//         localStorage.setItem("dark-mode", false)
+//         dbutton.innerHTML = "dark_mode"
+//     }
+//     darkButton.disabled = false
+// }
 
 darkButton.addEventListener("click", (e) => {
     e.preventDefault()
     darkButton.disabled = true
-    applyDarkTheme()
+    toggleTheme()
 })
+
+function setTheme(theme) {
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-mode', theme === 'dark');
+}
+
+function toggleTheme() {
+    const body = document.querySelector('body');
+    const isDarkMode = body.classList.contains('dark-mode');
+    setTheme(isDarkMode ? 'light' : 'dark');
+}
+
+// Detect user's system preference and set the theme accordingly
+setTheme(prefersDarkMode ? 'dark' : 'light');
+
+// Listen for changes in system preference and update the theme
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    setTheme(e.matches ? 'dark' : 'light');
+});
