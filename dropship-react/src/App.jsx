749eb977f66d2c984e6dec869668c2e4fbe5a99c
SarfDime <sarfdime@yahoo.com>
2023-09-17 22:22:19 +0200

    fix css

--- dropship-react/src/App.jsx
+++ dropship-react/src/App.jsx
@@ -8,6 +8,7 @@ import CategoriesList from './components/Categories/CategoriesList/CategoriesLis
 import CategoriesDetails from './components/Categories/CategoriesDetails/CategoriesDetails'
 import User from './components/User/User'
 import Cart from './components/Cart/Cart'
+import './styles.scss'
 
 import NotFound from './components/NotFound/NotFound'
 
