fe799de7790e7186d6c7d4b9f691195fa8c07306
SarfDime <sarfdime@yahoo.com>
2023-09-29 13:17:28 +0200

    s

--- dropship-react/src/store/store.js
+++ dropship-react/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./slices/user/userSlices";
 import { userSettingsReducer } from "./slices/userSettings/userSettingsSlices";
+import { productsReducer } from './slices/productsSlice'
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     userSettings: userSettingsReducer,
+    product:productsReducer
   },
 });
 
