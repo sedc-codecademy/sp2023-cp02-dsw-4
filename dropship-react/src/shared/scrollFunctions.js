53941a30cdd06ab005e12ed42c98859b1609231b
Ivan Ivanov <ivanovv7@icloud.com>
2023-10-24 19:39:22 +0200

    Section DONE

--- dropship-react/src/shared/scrollFunctions.js
+++
@@ -1,8 +0,0 @@
-export function applyNoScroll() {
-    if (!document.body.classList.contains("noScroll")) document.body.classList.add("noScroll")
-}
-
-export function applyScroll() {
-    if (document.body.classList.contains("noScroll")) document.body.classList.remove("noScroll")
-}
-
--- dropship-react/src/store/selectors/productSelector.js
+++
@@ -1,3 +0,0 @@
-export const selectProducts = (state) => state.product.products;
-
-export const selectTotalPrice = (state) => state.product.totalPrice;
